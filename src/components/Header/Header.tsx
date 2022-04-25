5717bbc4be51d294dd051bae745ff00b6b0523af
nastyanikitinteam <nastya@nikitinteam.com>
2022-04-25 23:06:31 +0200

    Edited styles in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import classnames from 'classnames';
+
 import styles from './header.module.scss';
 
 import Search from '../Search/Search';
@@ -6,6 +9,7 @@ import HelpIcon from '../../assets/images/icons/help.svg';
 import SlideImg1 from '../../assets/images/icons/icon_Help.png';
 
 const Header = () => {
+  const [isNewHelp, setIsNewHelp] = useState(true);
   return (
     <div className={styles.header}>
       <div className={styles.search}>
@@ -14,7 +18,9 @@ const Header = () => {
       <div className={styles.buttons}>
         <div className={styles.login}>Login</div>
         <div className={styles.registration}>Registration</div>
-        <div className={styles.help}>
+        <div
+          className={classnames(styles.help, { [styles.helpNew]: isNewHelp })}
+        >
           <img src={SlideImg1.src} alt="" />
         </div>
       </div>
