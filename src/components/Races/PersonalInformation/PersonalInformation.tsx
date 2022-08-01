f8e82980cd5836160e78b304f0848df6d657aeff
nastyanikitinteam <nastya@nikitinteam.com>
2022-08-01 09:30:14 +0200

    add personal information to competition

---
+++ src/components/Races/PersonalInformation/PersonalInformation.tsx
@@ -0,0 +1,46 @@
+import cn from 'classnames';
+
+import styles from './personalInformation.module.scss';
+
+import Avatar from '../../../assets/images/main/avatar.png';
+import BTC from '../../../assets/images/footer/BTC-color.png';
+
+const PersonalInformation = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.avatar}>
+          <img src={Avatar.src} alt="" />
+        </div>
+        Nikitin
+      </div>
+
+      <div className={styles.position}>
+        <p className={styles.description}>My Position</p>
+        <p className={styles.text}>50th+</p>
+      </div>
+      <div className={styles.position}>
+        <p className={styles.description}>Wagered</p>
+        <p className={styles.text}>
+          <span className={styles.icon}>
+            <img src={BTC.src} alt="" />
+          </span>
+          0.0000005
+        </p>
+      </div>
+      <div className={styles.position}>
+        <p className={styles.description}>
+          Wager to reach <span className={styles.green}>top 10</span>
+        </p>
+        <p className={styles.text}>
+          <span className={styles.icon}>
+            <img src={BTC.src} alt="" />
+          </span>
+          3.15002225
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalInformation;
--- src/components/Races/Сompetition/Competition.tsx
+++ src/components/Races/Сompetition/Competition.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
-
 import Timer from '../../Timer/Timer';
+import PersonalInformation from '../PersonalInformation/PersonalInformation';
 
 import Image from '../../../assets/images/main/competition-image.png';
 import BTC from '../../../assets/images/footer/BTC-color.png';
@@ -11,26 +10,28 @@ const Competition = () => {
 
   return (
     <div className={styles.content}>
-      <div className={styles.image}>
-        <img src={Image.src} alt="" />
+      <div className={styles.top}>
+        <div className={styles.image}>
+          <img src={Image.src} alt="" />
+        </div>
+        <div className={styles.price}>
+          <p>Contest prize pool</p>
+          <h3 className={styles.priceInfo}>
+            <span className={styles.icon}>
+              <img src={BTC.src} alt="" />
+            </span>
+            <span className={styles.yellow}>0.00134534</span> BTC
+          </h3>
+          <p className={styles.daily}>Daily</p>
+        </div>
+        <div className={styles.time}>
+          <p className={styles.description}>Time Remaining</p>
+          <Timer timer={timer} />
+        </div>
       </div>
-      <div className={styles.price}>
-        <p>Contest prize pool</p>
 
-        <h3 className={styles.priceInfo}>
-          <span className={styles.icon}>
-            <img src={BTC.src} alt="" />
-          </span>
-          <span className={styles.yellow}>0.00134534</span> BTC
-        </h3>
-
-        <p className={styles.daily}>Daily</p>
-      </div>
-
-      <div className={styles.time}>
-        <p className={styles.description}>Time Remaining</p>
-        <Timer timer={timer} />
-        <div className={styles.timer}></div>
+      <div className={styles.bottom}>
+        <PersonalInformation />
       </div>
     </div>
   );
