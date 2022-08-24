4d91b5828c06eeefed8c882a5fb7b254cb262f8d
nastyanikitinteam <nastya@nikitinteam.com>
2022-08-24 15:08:57 +0200

    global settings page - verify Level3

--- src/components/GlobalSettings/Verify/Level3/Level3.tsx
+++ src/components/GlobalSettings/Verify/Level3/Level3.tsx
@@ -1,7 +1,34 @@
+import cn from 'classnames';
+
+import Document from '../../Document/Document';
+
 import styles from './level3.module.scss';
 
 const Level3 = () => {
-  return <div className={styles.container}>Level3</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <p className={styles.error}>
+          Please complete level one verification first.
+        </p>
+        <p className={styles.text}>
+          Please upload supporting documentation for your Source of Funds and
+          Proof of Address. Documents lying on a flat surface must show all 4
+          corners and all information should be clear and identifiable.
+        </p>
+        <p className={styles.description}>
+          Following file types are accepted: .png, .jpg, .pdf
+        </p>
+        <div className={styles.item}>
+          <Document title="Source of Funds" />
+        </div>
+        <div className={styles.item}>
+          <Document title="Proof of Address" />
+        </div>
+        <div className={cn(styles.button, 'button button-sm')}>Upload</div>
+      </div>
+    </div>
+  );
 };
 
 export default Level3;
